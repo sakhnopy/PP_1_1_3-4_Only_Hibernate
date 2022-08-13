4a0a73432f7f3874a35acf6a8d217d27f87824c3
konkatenazia <41936055+sakhnopy@users.noreply.github.com>
2022-08-13 23:25:28 +0300

    Installed JDBC and SQL

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/Local instance MySQL80";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1121Regnomer1";
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Connection connection;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("connect OK");
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("connect no OK");
+        }
     }
 }
